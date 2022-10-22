3163da34731550fa024d7b7e02c4164cd182e99d
Kasper Hoffmann <kasp724s@edu.ucl.dk>
2022-10-22 23:15:00 +0200

    Add project files.

---
+++ MyHappy2Homes/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using MyHappy2Homes.ViewModels;
+using MyHappy2Homes.Views;
+using Xamarin.Forms;
+
+namespace MyHappy2Homes
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
