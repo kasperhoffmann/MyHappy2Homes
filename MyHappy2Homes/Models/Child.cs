e8770c417d081d8f9c75a6c877f6f1a49d4e13eb
Kasper Hoffmann <kasp724s@edu.ucl.dk>
2022-10-23 08:58:45 +0200

    Delete unused pages, make new pages and start making models

---
+++ MyHappy2Homes/Models/Child.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHappy2Homes.Models
+{
+    internal class Child
+    {
+    }
+}
---
+++ MyHappy2Homes/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Text;
+
+namespace MyHappy2Homes.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName 
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public int Age;
+
+    }
+}
